15f753f7c1c051f2dff81860bea021ec547859ca
Ula Chao <gary0626@gmail.com>
2022-01-02 17:05:27 +0800

    feat: impl search with debounce

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import debounce from 'lodash.debounce'
 import loadingGif from './loading.gif';
 import './App.css';
 import SearchBar from './Components/SearchBar';
@@ -7,33 +8,33 @@ import RepoList from './Components/RepoList';
 function App() {
   const [repoList, setRepoList] = React.useState([])
   const [totalCount, setTotalCount] = React.useState(0)
-  const [searchText, setSearchText] = React.useState('')
   const [loading, setLoading] = React.useState(false)
 
-  React.useEffect(() => {
-    async function fetchData() {
-      setLoading(true)
-      try {
-        const url = 'https://api.github.com/search/repositories'
-        const resp = await fetch(`${url}?q=${searchText}`)
-        const data = await resp.json()
-        setTotalCount(data.total_count)
-        setRepoList(data.items)
-        console.log('data====',data)
-      }catch(ex){
-        console.warn(ex)
-      }
-      setLoading(false)
-    }
+async function fetchData(keyword) {
+  setLoading(true)
+  try {
+    const url = 'https://api.github.com/search/repositories'
+    const query = `${ keyword ? `?q=${keyword}` : ''}`
+    const resp = await fetch(`${url}${query}`)
+    const data = await resp.json()
+    setTotalCount(data.total_count)
+    setRepoList(data.items)
+    // console.log('data====',data)
+  }catch(ex){
+    console.warn(ex)
+  }
+  setLoading(false)
+}
 
-    searchText!== '' && fetchData()
-    
-  }, [searchText])
+  const debounceSetKeyword = debounce((keyword) => {
+    fetchData(keyword)
+  }, 1000)
+
   return (
     <div className="App">
       <h1 className="header">Search Repository in Github</h1>
-      <SearchBar onChangeText={value => setSearchText(value)} value={searchText}/>
-      {searchText == '' && <p>Please insert the repo name</p>}
+      <SearchBar onSetKeyword={debounceSetKeyword}/>
+     
       {!!totalCount && <p>There are {totalCount} results.</p>}
       {loading && <img src={loadingGif} width={50}/>}
       <RepoList repoList={repoList}/>
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,10 +1,18 @@
+import React from 'react';
+
 function SearchBar (props) {
+	const [searchText, setSearchText] = React.useState('')
+
 	return(
 		<div className="search-bar">
 			<input
-				onChange={e => props.onChangeText(e.target.value)}
-				value={props.value}
+				onChange={e => {
+					props.onSetKeyword(e.target.value)
+					setSearchText(e.target.value)
+				}}
+				value={searchText}
 				/>
+				 {searchText == '' && <p>Please insert the repo name</p>}
 		</div>
 	)
 }
