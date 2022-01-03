62e554e5cb447d5688ef6f13bd737dbe63cd6d3e
Ula Chao <gary0626@gmail.com>
2022-01-03 23:13:27 +0800

    feat: err ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import debounce from 'lodash.debounce'
+import styled from 'styled-components';
 import { useHistory, useLocation } from "react-router-dom";
 import loadingGif from './loading.gif';
 import './App.css';
@@ -10,22 +11,34 @@ function App() {
   const [repoList, setRepoList] = React.useState([])
   const [totalCount, setTotalCount] = React.useState(null)
   const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState(null)
   const history = useHistory();
   let location = useLocation();
   let search = location.search.substring(3) // split `?q=`
-
+console.log('repoList', repoList)
   let page = 1
   async function fetchData() {
+    setError(null)
     setLoading(true)
     try {
-    const url = 'https://api.github.com/search/repositories'
-    const query = `?q=${search}?page=${page}`
-    const resp = await fetch(`${url}${query}`)
-    const data = await resp.json()
-    if(!data.items || !search || search === "") {
-      setTotalCount(null)
-      setRepoList([])
-    } else {
+      if( !search || search === "") {
+        setTotalCount(null)
+        setRepoList([])
+      } else {
+      const url = 'https://api.github.com/search/repositories'
+      const query = `?q=${search}?page=${page}`
+      const resp = await fetch(`${url}${query}`)
+      const data = await resp.json()
+      console.log('data?',data)
+      if(!data.items) {
+        if(data.message) {
+          setError(`${data.message}`)
+        } else {
+          setError('unknown error occurred') 
+        }
+        setLoading(false)
+        return
+      }
       let new_items = []
       data.items.forEach(item => new_items.push(item))
       setTotalCount(data.total_count)
@@ -46,7 +59,7 @@ function App() {
     let totalHeight = e.target.documentElement.scrollHeight 
     if(topOfScroll + window.innerHeight == totalHeight) {
       page = page + 1
-      fetchData({keyword: search})
+      debounce(() => fetchData({keyword: search}), 1000)()
     }
   }
 
@@ -55,14 +68,19 @@ function App() {
   },[])
 
   React.useEffect(() => {
+    setRepoList([]) 
     fetchData()
   },[search])
-
 
   return (
     <div className="App">
       <h1>Search Repository in Github</h1>
       <SearchBar onSetKeyword={debounceSetKeyword} paramsSearch={search}/>
+      {error && 
+      <ErrorBox>
+        <p>Some error has occurred.</p>
+        <div className='msg'>{error}</div>  
+      </ErrorBox>}
       {totalCount === 0 && <p>No matches were found.</p>}
       {!!totalCount && <p>{totalCount} results were found.</p>}
 
@@ -72,4 +90,18 @@ function App() {
   );
 }
 
+const ErrorBox = styled.div`
+  padding: 10px;
+  background-color: #ffa4a4;
+  color: #9d3939;
+  width: 50%;
+  margin: 20px auto;
+
+  & > .msg {
+    background-color: #eee;
+    padding: 15px;
+    margin-top: 20px;
+    line-height: 25px;
+  }
+`
 export default App;
